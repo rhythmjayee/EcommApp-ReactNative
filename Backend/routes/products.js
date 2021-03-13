84bb45625da58c8f983d5c6d85a87c486e78274b
Rhythm Jayee <jayeerythm8@gmail.com>
2021-03-13 15:38:17 +0530

    product post route

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -1,6 +1,8 @@
 import express from 'express';
 
 import Product from '../models/product.js';
+import Category from '../models/category.js';
+
 
 
 const router = express.Router();
@@ -22,17 +24,32 @@ router.get('/',async (req,res)=>{
 
 router.post('/', async (req,res)=>{
     try{
+            const category = await Category.findById(req.body.category);
+            if(!category){
+                return res.status(404).json({
+                    error:{message:'Invalid Category'},
+                    success:false
+                })
+            }
             const product = new Product({
                 name:req.body.name,
+                description:req.body.description,
+                richDescription:req.body.richDescription,
                 image:req.body.image,
-                countInStock:req.body.countInStock
+                brand:req.body.brand,
+                price:req.body.price,
+                category:req.body.category,
+                countInStock:req.body.countInStock,
+                rating:req.body.rating,
+                numReviews:req.body.numreviews,
+                isFeatured:req.body.isFeatured
             });
 
             const result = await product.save();
             if(!result){
-                return res.status(404).send('the product cannot be created');
+                return res.status(500).send('the product cannot be created');
             }
-            res.status(201).json(result);
+            res.status(201).send(result);
         }catch(err){
             res.status(500).json({
                 error:{message:'Error occured'},
