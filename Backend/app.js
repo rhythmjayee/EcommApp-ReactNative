4bc8a14a66ac2124b2c70350374a7b9b9771d06a
Rhythm Jayee <jayeerythm8@gmail.com>
2021-03-13 11:15:56 +0530

    db connected

--- Backend/app.js
+++ Backend/app.js
@@ -2,18 +2,38 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 
 
 dotenv.config({ silent: process.env.NODE_ENV === 'development' });
+mongoose.connect(process.env.DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    })
+    .then(con=>{
+        console.log('database connected!!')
+    })
+    .catch(err=>{
+        console.log('DB connection failed!!')
+    });
+
+
+// variables
 const api=process.env.API_URL;
 const app= express();
+
 
 
 //middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('tiny')); 
+
+
+
 
 app.get(`${api}/products`,(req,res)=>{
     const products={
