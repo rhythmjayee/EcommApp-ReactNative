837a45e975520fb4f5eac954b2641698e4e3524b
Rhythm Jayee <jayeerythm8@gmail.com>
2021-03-13 10:57:54 +0530

    added packages

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,33 @@
 // const express= require('express');
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+
+
+dotenv.config({ silent: process.env.NODE_ENV === 'development' });
+const api=process.env.API_URL;
 const app= express();
 
 
-app.get('/',(req,res)=>{
-    res.send("hello world!")
-})
+//middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan('tiny')); 
+
+app.get(`${api}/products`,(req,res)=>{
+    const products={
+        id:'sadfsdgs',
+        name:'table'
+    }
+    res.send(products);
+});
+
+app.post(`${api}/products`,(req,res)=>{
+    const newProduct=req.body;
+    console.log(newProduct);
+    res.send(newProduct);
+});
 
 
 
